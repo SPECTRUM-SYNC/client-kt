7a61246c23c1d406f34b17cb36b3736f1bfe88f1
Cabayer915 <caua.bonfim@sptech.school>
2024-09-25 21:58:11 -0300

    Adicionando tela de editar perfil

---
+++ fittech/app/src/main/java/spectrum/fittech/TelaEditarPerfil.kt
@@ -0,0 +1,330 @@
+package spectrum.fittech
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
+import spectrum.fittech.ui.theme.FittechTheme
+
+class TelaEditarPerfil : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FittechTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TelaEditarPerfil(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TelaEditarPerfil(name: String, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1C1C1E))
+            .padding(horizontal = 16.dp)
+            .padding(bottom = 16.dp),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            // Botão voltar
+            Box(
+                modifier = Modifier
+                    .size(56.dp)
+                    .background(Color(0xFF2C2C2E), shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("android.resource://spectrum.fittech/raw/setaesquerda")
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        ),
+                        contentDescription = "Home",
+                        modifier = Modifier.size(24.dp),
+                        tint = Color.White
+                    )
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Imagem com borda circular
+            Box(
+                modifier = Modifier
+                    .size(120.dp)
+                    .border(
+                        BorderStroke(1.dp, Color.Unspecified),
+                        shape = CircleShape
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.mipmap.dalva),
+                    contentDescription = "user",
+                    modifier = Modifier
+                        .size(120.dp)
+                        .clip(CircleShape)
+                )
+
+                // Ícone de câmera no canto inferior direito
+                Box(
+                    modifier = Modifier
+                        .size(35.dp)
+                        .align(Alignment.BottomEnd) // Alinhamento no canto inferior direito
+                        .background(Color(0xFF2C2C2E), CircleShape), // Fundo cinza circular para o ícone
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("android.resource://spectrum.fittech/raw/nutri")
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        ),
+                        contentDescription = "Nutri",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        }
+
+
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            // Primeira linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+
+            // Primeira Row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_editar_perfil),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("android.resource://spectrum.fittech/raw/setadireita")
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        ),
+                        contentDescription = "Editar Perfil",
+                        modifier = Modifier.size(16.dp),
+                        tint = Color.White
+                    )
+                }
+            }
+
+            // Segunda linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+
+            // Segunda Row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_politica_privacidade),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("android.resource://spectrum.fittech/raw/setadireita")
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        ),
+                        contentDescription = "Politica de Privacidade",
+                        modifier = Modifier.size(16.dp),
+                        tint = Color.White
+                    )
+                }
+            }
+
+            // Terceira linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+
+            // Terceira Row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_configuracoes),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                IconButton(onClick = { }) {
+                    Icon(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data("android.resource://spectrum.fittech/raw/setadireita")
+                                .decoderFactory(SvgDecoder.Factory())
+                                .build()
+                        ),
+                        contentDescription = "Configuracoes",
+                        modifier = Modifier.size(16.dp),
+                        tint = Color.White
+                    )
+                }
+            }
+
+            // Quarta linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+        }
+
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            // Primeira linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+
+            // Row Sair
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp)
+                    .padding(vertical = 20.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.txt_sair),
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        color = Color(0xFFFF2424),
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+
+            // Segunda linha
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Color(0xFF2C2C2E))
+            )
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TelaEditarPerfilPreview() {
+    FittechTheme {
+        TelaEditarPerfil("Android")
+    }
+}
