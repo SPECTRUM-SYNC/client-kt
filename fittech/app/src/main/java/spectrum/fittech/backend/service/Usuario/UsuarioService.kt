777d15c94d5e06e03d70e52354579c364e797877
RicardoVicentepc <ricardo.vicente@sptech.school>
2024-09-27 19:25:20 -0300

    reajuste em usuario

--- fittech/app/src/main/java/spectrum/fittech/backend/service/Usuario/UsuarioService.kt
+++ fittech/app/src/main/java/spectrum/fittech/backend/service/Usuario/UsuarioService.kt
@@ -1,30 +1,17 @@
 package spectrum.fittech.backend.service.Usuario
 
 import retrofit2.Call
-<<<<<<< HEAD
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import spectrum.fittech.backend.dtos.*
-import spectrum.fittech.backend.builder.gson
-import spectrum.fittech.backend.dtos.Usuario
-import spectrum.fittech.backend.interfaces.ApiInterface
-=======
 import retrofit2.Callback
->>>>>>> c74bd203dfe415fcac67c2cd3963e08ec6613750
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import spectrum.fittech.backend.Object.IdUserManager
 import spectrum.fittech.backend.Object.TokenManager
-import spectrum.fittech.backend.builder.gson
-import spectrum.fittech.backend.dtos.AdicionarTopRank
-import spectrum.fittech.backend.dtos.AtivarUsuario
-import spectrum.fittech.backend.dtos.AtualizarImagem
 import spectrum.fittech.backend.dtos.AtualizarUsuario
 import spectrum.fittech.backend.dtos.AtualizarUsuarioPerfil
 import spectrum.fittech.backend.dtos.EnvioEmailUsuario
-import spectrum.fittech.backend.dtos.Objetivo
 import spectrum.fittech.backend.dtos.RespostaCadastro
+import spectrum.fittech.backend.mock.*
 import spectrum.fittech.backend.dtos.RespostaEnvioEmail
 import spectrum.fittech.backend.dtos.RespostaLogin
 import spectrum.fittech.backend.dtos.RespostaRank
@@ -35,104 +22,13 @@ import spectrum.fittech.backend.dtos.UsuarioLogin
 import spectrum.fittech.backend.dtos.UsuarioLoginGoogle
 import spectrum.fittech.backend.interfaces.ApiInterface
 import spectrum.fittech.backend.log.client
-import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.util.Date
-import javax.security.auth.callback.Callback
-
-
-val usuario = Usuario(
-    nome = "Rafael Reis",
-    email = "Ricardo.vicesssnsste.asssasd@sptech.school",
-    senha = "Madalena13#",
-    img = "string"
-)
-
-val formatoData = SimpleDateFormat("yyyy-MM-dd")
-val dataNascimento = formatoData.format(Date(90, 5, 15))
-// Criando uma instância da classe AtualizarUsuario com valores fictícios
-val usuarioAtualizar = AtualizarUsuario(
-    nome = "João Silva",
-    senha = "9242#Rick",
-    img = "https://exemplo.com/imagem.png",
-    genero = "Masculino",
-    peso = 95.5,
-    altura = 175,
-    dataNascimento = "2001-01-10",
-    meta = "Perder peso",
-    nivelCondicao = "Intermediário"
-)
-
-val usuarioAtualizarPerfil = AtualizarUsuarioPerfil(
-    nome = "João Silva",
-    altura = 175,
-    dataNascimento = "2001-01-10",
-    meta = "Perder peso",
-    nivelCondicao = "Intermediário"
-)
-
-val objetivo = Objetivo(
-    objetivo = "Ganho de Massa"
-)
-
-val usuarioAtivar =  AtivarUsuario(
-    nome = "Lula Molusco",
-    email = "FazOL@gmail.com",
-    img = "SiriSacudo",
-    meta = "Ganho de Massa",
-    dataNascimento = LocalDateTime.now(),
-    genero = "GAY",
-    peso = 20.0,
-    altura =  20.0,
-    nivelCondicao = "GORDOLASCADOMAIORQUESUAMAE",
-    contaAtiva = true,
-    pontuacao = 200000.0,
-    objetivo = objetivo
-)
-
-val atualizarImagemUsuario =  gson.toJson(AtualizarImagem(
-    imageFile = "profile.png"
-))
-
-val usuarioLogin = UsuarioLogin(
-    email = "winycios@gmail.com",
-    senha = "Madalena13#"
-)
-val usuarioLoginGoogle = UsuarioLoginGoogle(
-    email = "winycios@gmail.com",
-    nome = "Madalena13#"
-)
-
-val usuarioEnvioEmail = EnvioEmailUsuario(
-    para = "Ricardo.vicesssnsste.asssasd@sptech.school",
-    nome = "Vinicius13"
-)
-
-
-val userTopRank =  gson.toJson(AdicionarTopRank(
-    nome = "Lula Molusco",
-    email = "FazOL@gmail.com",
-    senha = "boqueteParafuso",
-    img = "SiriSacudo",
-    meta = "Ganho de Massa",
-    dataNascimento = LocalDateTime.now(),
-    genero = "GAY",
-    peso = 20.0,
-    altura =  20.0,
-    nivelCondicao = "GORDOLASCADOMAIORQUESUAMAE",
-    contaAtiva = true,
-    pontuacao = 200000.0,
-    horaSenhaAtualizacao = LocalDateTime.now(),
-    objetivo = objetivo
-))
-
 
 fun main() {
     //cadastrarUsuario(usuario);
     //loginUsuarioGoogle(usuarioLoginGoogle);
     //envioEmail(usuarioEnvioEmail);
-    //loginUsuario(usuarioLogin) { success ->
-      //  if (success) {
+    loginUsuario(usuarioLogin) { success ->
+        if (success) {
             //adicionarRanking(usuario = userTopRank, token = "Bearer ${TokenManager.token.toString()}")
             //ativarUsuario(id = IdUserManager.userId, usuario = usuarioAtivar, token = "Bearer ${TokenManager.token.toString()}" )
             //atualizarImagem(id = IdUserManager.userId, token = "Bearer ${TokenManager.token.toString()}", atualizarImagemUsuario)
@@ -140,17 +36,152 @@ fun main() {
             //atualizarUsuarioPerfil(id = IdUserManager.userId, token = "Bearer ${TokenManager.token.toString()}", usuarioAtualizarPerfil)
             //atualizarUsuarioPontuacao(id = IdUserManager.userId, token = "Bearer ${TokenManager.token.toString()}")
             //obterUsuario(id = IdUserManager.userId, token = "Bearer ${TokenManager.token.toString()}")
-    //    } else {
-      //      println("Login falhou")
-       // }
-    //}
-
-    obterTodosUsuarios() { resultado ->
-        println(resultado)  // Resultado será impresso aqui
+            //obterUsuarioStatusAtivo(contaAtiva = true, token = "Bearer ${TokenManager.token.toString()}")
+            //obterRankUsuariosTop3(token = "Bearer ${TokenManager.token.toString()}")
+            //obterUsuarioOrdemAlfabetica(token = "Bearer ${TokenManager.token.toString()}")
+            //excluirUsuario(id = 2, token = "Bearer ${TokenManager.token.toString()}")
+            //inativarUsuario(id = 1, token = "Bearer ${TokenManager.token.toString()}")
+        } else {
+            println("Login falhou")
+        }
     }
+
+
+}
+
+// DELETE
+fun inativarUsuario(id: Int?, token: String?) {
+    val interfaceUsuario = gerarAmbiente();
+
+    val call = interfaceUsuario.inativarUsuario(id = id, token = token)
+
+    call.enqueue(object : Callback<RespostaRequisicao> {
+        override fun onResponse(
+            call: Call<RespostaRequisicao>,
+            response: Response<RespostaRequisicao>
+        ) {
+            if(response.isSuccessful){
+                println("status : ${response.code()}")
+                //println("Corpo : ${response.body()}")
+            } else {
+                println("Erro ao realizar consulta Usuario: ${response.body()}")
+            }
+        }
+
+        override fun onFailure(call: Call<RespostaRequisicao>, t: Throwable) {
+            println("Falha na obtenção Usuário. Código de resposta: ${t.message}")
+            return
+        }
+    })
+}
+
+fun excluirUsuario(id: Int?, token: String?) {
+    val interfaceUsuario = gerarAmbiente();
+
+    val call = interfaceUsuario.excluirUsuario(id = id, token = token)
+
+    call.enqueue(object : Callback<RespostaRequisicao> {
+        override fun onResponse(
+            call: Call<RespostaRequisicao>,
+            response: Response<RespostaRequisicao>
+        ) {
+            if(response.isSuccessful){
+                println("status : ${response.code()}")
+                //println("Corpo : ${response.body()}")
+            } else {
+                println("Erro ao realizar consulta Usuario: ${response.body()}")
+            }
+        }
+
+        override fun onFailure(call: Call<RespostaRequisicao>, t: Throwable) {
+            println("Falha na obtenção Usuário. Código de resposta: ${t.message}")
+            return
+        }
+    })
 }
 
 // GETs
+
+fun obterUsuarioOrdemAlfabetica(token: String?) {
+    val interfaceUsuario = gerarAmbiente();
+
+    val call = interfaceUsuario.obterUsuarioOrdemAlfabetica(token = token)
+
+    call.enqueue(object : Callback<List<UsuarioGet>> {
+        override fun onResponse(
+            call: Call<List<UsuarioGet>>,
+            response: Response<List<UsuarioGet>>
+        ) {
+            if(response.isSuccessful){
+                println("status : ${response.code()}")
+                //println("Corpo : ${response.body()}")
+                val usuario: List<UsuarioGet>? = response.body()
+                println("usuario no obj = " + usuario);
+            } else {
+                println("Erro ao realizar consulta Usuario: ${response.body()}")
+            }
+        }
+
+        override fun onFailure(call: Call<List<UsuarioGet>>, t: Throwable) {
+            println("Falha na obtenção Usuário. Código de resposta: ${t.message}")
+            return
+        }
+    })
+}
+
+fun obterRankUsuariosTop3(token : String?) {
+    val interfaceUsuario = gerarAmbiente();
+
+    val call = interfaceUsuario.obterUsuarioPontuacaoTop3(token = token)
+
+    call.enqueue(object : Callback<List<UsuarioGet>> {
+        override fun onResponse(
+            call: Call<List<UsuarioGet>>,
+            response: Response<List<UsuarioGet>>
+        ) {
+            if(response.isSuccessful){
+                println("status : ${response.code()}")
+                //println("Corpo : ${response.body()}")
+                val usuario: List<UsuarioGet>? = response.body()
+                println("usuario no obj = " + usuario);
+            } else {
+                println("Erro ao realizar consulta Usuario: ${response.body()}")
+            }
+        }
+
+        override fun onFailure(call: Call<List<UsuarioGet>>, t: Throwable) {
+            println("Falha na obtenção Usuário. Código de resposta: ${t.message}")
+            return
+        }
+    })
+}
+
+fun obterUsuarioStatusAtivo(contaAtiva : Boolean?, token : String?) {
+    val interfaceUsuario = gerarAmbiente();
+
+    val call = interfaceUsuario.obterStatusUsuario(contaAtiva =  contaAtiva, token = token)
+
+    call.enqueue(object : Callback<List<UsuarioGet>> {
+        override fun onResponse(
+            call: Call<List<UsuarioGet>>,
+            response: Response<List<UsuarioGet>>
+        ) {
+            if(response.isSuccessful){
+                println("status : ${response.code()}")
+                //println("Corpo : ${response.body()}")
+                val usuario: List<UsuarioGet>? = response.body()
+                println("usuario no obj = " + usuario);
+            } else {
+                println("Erro ao realizar consulta Usuario: ${response.body()}")
+            }
+        }
+
+        override fun onFailure(call: Call<List<UsuarioGet>>, t: Throwable) {
+            println("Falha na obtenção Usuário. Código de resposta: ${t.message}")
+            return
+        }
+    })
+}
 
 fun obterUsuario(id: Int?, token: String?) {
     val interfaceUsuario = gerarAmbiente();
@@ -181,11 +212,7 @@ fun obterUsuario(id: Int?, token: String?) {
 
 }
 
-
-private fun <T> Call<T>.enqueue(callback: Callback<List<T>>) {}
-
 // PUTs
-
 fun atualizarUsuarioPontuacao(id: Int?, token: String?) {
     val interfaceUsuario = gerarAmbiente();
 
@@ -263,8 +290,6 @@ fun atualizarUsuarioPerfil(id: Int?, token: String?, usuario: AtualizarUsuarioPe
 
 }
 
-
-
 // PATCHS
 fun atualizarImagem(id: Int?, token: String?, imagem : String) {
     val interfaceUsuario = gerarAmbiente();
@@ -309,12 +334,10 @@ fun ativarUsuario(id: Int?, usuario: String, token: String?) {
 
         override fun onFailure(call: Call<RespostaRequisicao>, t: Throwable) {
             println("Falha na requisição. Código de resposta: ${t.message}")
-            return
         }
     })
 
 }
-
 
 // POSTS
 fun adicionarRanking(usuario: String, token:String) {
@@ -326,15 +349,12 @@ fun adicionarRanking(usuario: String, token:String) {
         override fun onResponse(call: Call<RespostaRank>, response: Response<RespostaRank>){
             if(response.isSuccessful) {
                 println("Resposta : ${response.body()}")
-                return
             } else {
                 println("Ocorreu um erro ao cadastrar no rank : ${response.code()}")
-                return
             }
         }
         override fun onFailure(call: Call<RespostaRank>, t: Throwable) {
             println("Falha na requisição. Código de resposta: ${t.message}")
-            return
         }
     })
 
@@ -349,15 +369,12 @@ fun envioEmail(usuario:EnvioEmailUsuario) {
         override fun onResponse(call: Call<RespostaEnvioEmail>, response: Response<RespostaEnvioEmail>){
             if(response.isSuccessful) {
                 println("${response.body()}")
-                return
             } else {
                 println("Ocorreu um erro ao enviar email : ${response.code()}")
-                return
             }
         }
         override fun onFailure(call: Call<RespostaEnvioEmail>, t: Throwable) {
             println("Falha na requisição. Código de resposta: ${t.message}")
-            return
         }
     })
 }
@@ -388,7 +405,6 @@ fun loginUsuario(usuario:UsuarioLogin, callback: (Boolean) -> Unit){
 
         override fun onFailure(call: Call<RespostaLogin>, t: Throwable) {
             println("Falha na requisição. Código de resposta: ${t.message}")
-            return
         }
     })
 }
@@ -432,10 +448,8 @@ fun cadastrarUsuario(usuario:Usuario) {
             if (response.isSuccessful) {
                 if (response.code() == 201) {
                     println("Cadastro realizado com sucesso: ${response.code()}")
-                    return;
                 } else {
                     println("Cadastro não foi realizado com sucesso. Código de resposta: ${response.code()}")
-                    return;
                 }
             } else {
                 println("Falha na requisição. Código de resposta: ${response.code()}")
@@ -449,9 +463,6 @@ fun cadastrarUsuario(usuario:Usuario) {
         }
     })
 }
-
-
-
 
 
 /*
